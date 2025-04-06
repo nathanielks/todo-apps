a2f32262e05d32e98624ac241a13bf16d0e51b61
Nathaniel <314009+nathanielks@users.noreply.github.com>
2025-04-05 20:23:31 -0400

    add watch behavior to restart api, store todos ephemerally

--- api/express/src/index.ts
+++ api/express/src/index.ts
@@ -1,23 +1,28 @@
 import express from "express";
+import { ulid } from "ulid";
+import bodyParser from "body-parser";
+
 const app = express();
-const port = 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const z = () => 0;
-
-const todo = {
-	title: "[Bug] report 1",
-	body: "Bug description",
-};
+// Ephemerally store things for now
+const todos = new Map();
 
 app.get("/todos", (req, res) => {
-	res.json([todo]);
+  res.json(Array.from(todos.values()));
 });
 
 app.post("/todos", (req, res) => {
-	res.status(201).send(todo);
+  const id = ulid();
+  const todo = todos.set(id, {
+    ...req.body,
+    id,
+  });
+  res.status(201).send(todo);
 });
 
+const port = process.env.API_PORT ?? 3000;
 app.listen(port, () => {
-	z();
-	console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });

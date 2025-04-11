2f54cbd79edf21207b9ade98efe90d2fd99070be
Nathaniel <314009+nathanielks@users.noreply.github.com>
2025-04-11 09:22:18 -0400

    update test

--- api/fastify/test/routes/todos.test.ts
+++ api/fastify/test/routes/todos.test.ts
@@ -2,12 +2,12 @@ import { test } from "node:test";
 import * as assert from "node:assert";
 import { build } from "../helper.js";
 
-test("example is loaded", async (t) => {
-	const app = await build(t);
+test("lists todos", async (t) => {
+  const app = await build(t);
 
-	const res = await app.inject({
-		url: "/example",
-	});
+  const res = await app.inject({
+    url: "/todos/",
+  });
 
-	assert.equal(res.payload, "this is an example");
+  assert.equal(res.payload, "[]");
 });
